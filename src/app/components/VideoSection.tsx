213ba48b1c7c29d2f2bbecb65480496e3680ac33
rossduris <rossduris@gmail.com>
2024-06-02 15:39:39 -0400

    updated styles

--- src/app/components/VideoSection.tsx
+++ src/app/components/VideoSection.tsx
@@ -21,25 +21,26 @@ const VideoSection = () => {
   }, []);
 
   return (
-    <>
-      <video
-        style={{
-          objectFit: "cover",
-        }}
-        width="100%"
-        height="100%"
-        autoPlay
-        muted
-        loop
-      >
-        <source
-          src="https://theohtv.s3.amazonaws.com/carvideo.mp4"
-          type="video/mp4"
-        />
-        {`Your browser does not support the video tag.`}
-      </video>
-
-      <section className="absolute top-0 min-h-screen flex border-t border-black border-b w-full h-full justify-center items-center">
+    <div className=" relative">
+      <section className="relative top-0 min-h-screen flex border-t border-black border-b w-full h-full justify-center items-center">
+        <div className=" w-full flex absolute h-screen">
+          <video
+            style={{
+              opacity: 0.4,
+            }}
+            width="100%"
+            height="100%"
+            autoPlay
+            muted
+            loop
+          >
+            <source
+              src="https://theohtv.s3.amazonaws.com/carvideo.mp4"
+              type="video/mp4"
+            />
+            {`Your browser does not support the video tag.`}
+          </video>
+        </div>
         <div>
           <h1
             className={`font-bold md:text-8xl max-w-[400px] mt-20 -ml-14 sm:-ml-20 w-[300px] text-6xl relative `}
@@ -72,7 +73,7 @@ const VideoSection = () => {
           </Link>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
