68628da5330f8cc870ea2108b46ba4191b2d9bd6
rossduris <rossduris@gmail.com>
2024-06-02 15:51:32 -0400

    updated styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import ServicesSection from "./components/ServicesSection";
 import MapSection from "./components/MapSection";
 import AboutUsSection from "./components/AboutUsSection";
 import VideoSection from "./components/VideoSection";
-import GallerySection from "./components/GallerySection";
 import CarCompanies from "./components/CarCompanies";
-import ImageSlider from "./components/ImageSlider";
 import FlexGallery from "./components/FlexGallery";
 
 export default function Home() {
